04777783d12fec983b8496eca7e87f25c9a8b279
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-09-07 11:29:43 +0530

    Update Insertionsort.java

--- Insertionsort.java
+++ Insertionsort.java
@@ -1,1 +1,34 @@
+import java.lang.*;
 
+public class First {
+    public static void insertionsort(int[] arr)
+    {
+        int n = arr.length;
+        for(int i=1;i<n;i++)
+        {
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]<key)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {25,67,89,34,10,23,12};
+        insertionsort(arr);
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+
+
+
+    }
+
+
+    }
