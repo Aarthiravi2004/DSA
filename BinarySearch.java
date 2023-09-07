29488c17e8d82045d4828144738377e4837a1fed
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-09-07 12:15:50 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.*;
+
+public class First {
+    public static int binarysearch(int[] arr, int x)
+    {
+        int l=0;
+        int r=(arr.length-1);
+        while(l<r)
+        {
+            int mid=(l+r)/2;
+            if(arr[mid]==x)
+            {
+                return mid;
+            }
+            else if(x < arr[mid])
+            {
+                r = mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = {25,67,89,34,10,23,12};
+        int x=92;
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.print(binarysearch(arr,x));
+
+    }
+ }
