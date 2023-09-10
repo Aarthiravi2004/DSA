4425b2be88407d5b1183723521bddbaabe5a1fb8
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-09-10 12:26:59 +0530

    Update ToFind NumberofDigits.java

--- ToFind NumberofDigits.java
+++ ToFind NumberofDigits.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             int k=0;
-            k=(int)Math.log10(nums[i])+1;
+            k=(int)Math.log10(nums[i])+1; //to find the number of digits in the decimal format ex:log2(nums[i]) --> it will return the digits in the binary format
             if(k%2==0)
             {
                 count++;
