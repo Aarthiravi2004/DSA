97cd9225bc9ded28d9425b2ad0b8fe4b017f55ad
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-10-21 11:55:55 +0530

    Create 2D-BinarySearch.java

---
+++ binarysearch/2D-BinarySearch.java
@@ -0,0 +1,44 @@
+if the array is sorted in row wise and column wise then this binary search algorithm is find useful
+
+
+Ans:
+
+class Main{
+    public static void main(String[] args)
+    {
+        int[][] arr = {
+            {10,20,30,40},
+            {15,25,35,45},
+            {28,29,37,49},
+            {33,34,38,50}
+        };
+        
+        System.out.println(Arrays.toString(search(arr,49)));
+                
+    }
+    static int[] search(int[][] arr, int target)
+    {
+        int r=0;
+        int c = arr.length-1;
+        while(r<arr.length && c>=0)
+        {
+            if(arr[r][c]==target)
+            {
+                return new int[]{r,c};
+            }
+            else if(arr[r][c] < target)
+            {
+                r++;
+            }
+            else
+            {
+                c--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+
+
+Output:
+[2,3]
