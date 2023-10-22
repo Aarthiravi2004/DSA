0009a1cfa95052533db045b7140aee464a29e0b0
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-10-22 19:06:32 +0530

    Update cyclicSorting.java

--- Sorting/cyclicSorting.java
+++ Sorting/cyclicSorting.java
@@ -1,1 +1,35 @@
 when the given rage is from 1 to N then use the cyclic sorting algorithm //very important
+
+
+  Source Code:
+
+class Main{
+    public static void main(String[] args)
+    {
+        int[] arr={8,7,6,5,1,2,3,4};
+        cyclicSorting(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+
+ static void cyclicSorting(int[] arr)
+{
+    int i=0;
+    while(i < arr.length){              //while loop for the continuous swapping until that particular position get the appropriate number
+        int correct = arr[i]-1;
+        if(i != correct)
+        {
+            swap(arr,i,correct);
+        }
+        else 
+        {
+            i++;
+        }
+    }
+}
+ static void swap(int[] arr, int first, int second)
+{
+    int temp = arr[first];
+    arr[first]=arr[second];
+    arr[second]=temp;
+}
+}
