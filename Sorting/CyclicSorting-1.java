bf603f0502259026ab826c1f0efb899c4046b27f
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-10-22 19:45:53 +0530

    Create CyclicSorting-1.java

---
+++ Sorting/CyclicSorting-1.java
@@ -0,0 +1,35 @@
+No:448  
+https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] arr) {
+    int i=0;
+    while(i < arr.length){
+        int correct = arr[i]-1;
+        if(arr[i] != arr[correct])
+        {
+            swap(arr,i,correct);
+        }
+        else 
+        {
+            i++;
+        }
+    }
+    List<Integer> ans = new ArrayList<>();
+    for(int j=0;j<arr.length;j++)
+    {
+        if(arr[j] != j+1)
+        {
+            ans.add(j+1);
+        }
+    }
+    return ans;
+}
+ static void swap(int[] arr, int first, int second)
+{
+    int temp = arr[first];
+    arr[first]=arr[second];
+    arr[second]=temp;
+}
+}
