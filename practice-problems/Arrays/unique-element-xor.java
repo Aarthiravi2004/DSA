9b3ef5bfa70bfa2e8199561a8ddbaaa68eea84d6
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2024-03-05 18:39:19 +0530

    Delete practice-problems/Arrays/unique-element-xor.java

--- practice-problems/Arrays/unique-element-xor.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int number = 0;
-        for(int i = 0 ; i < nums.length ;i++){
-            number = number ^ nums[i];
-        }
-        return number ;
-    }
-}
